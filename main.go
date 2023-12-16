bc406a053bc62ee10c0a2558997f3ed5ec417f67
Erfan <erfanmomeny@gmail.com>
2023-12-16 22:46:00 +0330

    Update main.go

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ const BASE_URL = "https://www.xe.com/currencytables"
 func main() {
 	e := echo.New()
 	e.GET("/symbols", GetSymbols)
-	e.GET("/rates", GetRates)
+	e.GET("/:date", GetRates)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -48,42 +48,25 @@ func GetSymbols(e echo.Context) error {
 }
 
 func GetRates(e echo.Context) error {
-	date := time.Now().Add(-24 * time.Hour).Format("2006-01-02")
-	if e.QueryParam("date") != "" {
-		date = e.QueryParam("date")
-	}
-	base := "USD"
-	if e.QueryParam("base") != "" {
-		base = e.QueryParam("base")
-	}
-	symbols := []string{}
-	if e.QueryParam("symbols") != "" {
-		symbols = strings.Split(e.QueryParam("symbols"), ",")
-	}
 	response := new(RatesResponse)
 	rates := make(map[string]float64)
 	c := colly.NewCollector()
 	c.SetRequestTimeout(60 * time.Second)
 	c.OnHTML("table.currencytables__Table-sc-xlq26m-3 > tbody", func(r *colly.HTMLElement) {
 		r.ForEach("tr", func(_ int, r *colly.HTMLElement) {
-			if len(symbols) == 0 {
-				currency, _ := strconv.ParseFloat(r.ChildText("td:nth-child(3)"), 64)
-				rates[r.ChildText("*[scope]")] = currency
-			} else {
-				for _, s := range symbols {
-					if s == r.ChildText("*[scope]") {
-						currency, _ := strconv.ParseFloat(r.ChildText("td:nth-child(3)"), 64)
-						rates[r.ChildText("*[scope]")] = currency
-					}
+			for _, s := range strings.Split(e.QueryParam("symbols"), ",") {
+				if s == r.ChildText("*[scope]") {
+					currency, _ := strconv.ParseFloat(r.ChildText("td:nth-child(3)"), 64)
+					rates[r.ChildText("*[scope]")] = currency
 				}
 			}
 		})
 		response = &RatesResponse{
-			Base:  base,
-			Date:  date,
+			Base:  e.QueryParam("base"),
+			Date:  e.Param("date"),
 			Rates: rates,
 		}
 	})
-	c.Visit(fmt.Sprintf("%s/?from=%s&date=%s", BASE_URL, base, date))
+	c.Visit(fmt.Sprintf("%s/?from=%s&date=%s", BASE_URL, e.QueryParam("base"), e.Param("date")))
 	return e.JSON(http.StatusOK, response)
 }
